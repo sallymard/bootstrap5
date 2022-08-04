ba0c03e2a125d39c055ba291f3fde4d373b17410
sallymard <sallymardini1999@gmail.com>
2022-08-04 16:08:20 +0300

    adding js

---
+++ js.js
@@ -0,0 +1,21 @@
+function Focus(x) {
+    x.style.background = "white";
+  }
+  function Upper() {
+    var x = document.getElementById("fname");
+    x.value = x.value.toUpperCase();
+  }
+
+  function check_email() {
+   alert("Thanks for Contact Us Chack Your Email");
+  }
+  
+  function bigImg(x) {
+    x.style.height = "350x";
+    x.style.width = "450px";
+  }
+  
+  function normalImg(x) {
+    x.style.height = "300px";
+    x.style.width = "400px";
+  }
